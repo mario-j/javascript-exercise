cce3424933073383f50be02dc460b56aaadbb3ad
Mario Jordan <mjordanbartolo@gmail.com>
2021-07-29 23:13:43 -0500

    Add exercise 2

--- exercise.js
+++ exercise.js
@@ -1,6 +1,6 @@
-var inputOne= [{"id": 1, "name": "Alice", "score": 60}, {"id": 2, "name": "Bob", "score": 50}, {"id": 1, "name": "Alice", "score": 50}, {"id": 3, "name": "", "score": 100}, {"id": 4, "name": "Bob", "score": 60}, {"name": "Bob", "score": 90}, {"id": 5, "name": "Charles", "score": 51}, {"id": 5, "name": "Charles", "score": 99}]
+//Exercise 1
 
-var inputTwo = [{"id": 1, "name": "Alice", "score": 60}, {"id": 4, "name": "Bob", "score": 60}, {"id": 5, "name": "Charles", "score": 51}]
+var exampleInput = [{"id": 1, "name": "Alice", "score": 60}, {"id": 2, "name": "Bob", "score": 50}, {"id": 1, "name": "Alice", "score": 50}, {"id": 3, "name": "", "score": 100}, {"id": 4, "name": "Bob", "score": 60}, {"name": "Bob", "score": 90}, {"id": 5, "name": "Charles", "score": 51}]
 
 function cleanObjArray(objArray) {
 	var sorted = objArray.sort(compare); 
@@ -25,6 +25,38 @@ function compare( a, b ) {
   return 0;
 }
 
+console.log("Exercise 1 Test Input 1", cleanObjArray(exampleInput));
 
-console.log("Example Input 1", cleanObjArray(inputOne));
-console.log("Example Input 2", cleanObjArray(inputTwo));
+//Exercise 2
+function scoreFunction(type, id, score) {
+	var scoreArray = [{"id": 1, "score": 1}, { "id": 2, "score": 2}];
+	switch(type) {
+		case "create":
+			var newId = Math.max.apply(Math, scoreArray.map(function(o) { return o.id; })) + 1;
+			var objToAdd = { id: newId, score: score };
+			scoreArray.push(objToAdd);
+			return scoreArray;
+			break;
+		case "read":
+			return scoreArray.filter(x => x.id == id)[0];
+			break;
+		case "update":
+			return scoreArray.map(function (o) { if (o.id == id) { o.score = score; } return o});
+			break;
+		case "delete":
+			return scoreArray.filter(x => x.id != id);
+			break;
+		default:
+			return scoreArray;
+	}
+}
+ 
+console.log("Exercise 2 Test Input 1 (Create)", scoreFunction("create", null, 3));
+
+console.log("Exercise 2 Test Input 2 (Read)", scoreFunction("read", 2, null));
+
+console.log("Exercise 2 Test Input 3 (Update)", scoreFunction("update", 2, 3));
+
+console.log("Exercise 2 Test Input 4 (Delete)", scoreFunction("delete", 2, null));
+
+
